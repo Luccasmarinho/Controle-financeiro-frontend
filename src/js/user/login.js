881b2ff9618ce68ceba0bdf3434dd43241037582
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2024-09-27 18:01:45 -0300

    teste

--- src/js/user/login.js
+++ src/js/user/login.js
@@ -12,19 +12,28 @@ function redir() {
 
 document.getElementById("link-cadastrar").addEventListener("click", redir)
 
+function guardarToken(token) {
+    localStorage.setItem("token", JSON.stringify(token))
+}
 
 formularioLogin.addEventListener("submit", async (event) => {
     event.preventDefault()
     const buscaApi = await api.login(inputEmail.value, inputSenha.value)
-    // console.log(buscaApi.conexaoConvertida.token)
 
     if (!buscaApi) {
         return
     } else if (buscaApi.statusCode == 200) {
+        const { token } = buscaApi.conexaoConvertida
+        const { id } = buscaApi.conexaoConvertida.user
+        guardarToken(token)
         modal.style.display = "flex"
 
-        setTimeout(() => {
-            window.location.href = "./src/pages/home.html"
-        }, 1500)
+        // setTimeout(() => {
+        //     window.location.href = "./src/pages/home.html"
+        // }, 1500)
     }
 })
+
+if(localStorage.getItem("token")) {
+    window.location.href = "./src/pages/home.html"
+}
